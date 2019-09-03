7aae9d75ce7c3dc4584dc994b61ab620306c3ad4
wdy0808 <wdy0808@outlook.com>
2019-09-03 10:52:34 +0800

    fix invalid sql statement

--- doc/sql/create_table.sql
+++ doc/sql/create_table.sql
@@ -1,30 +1,31 @@
+use blog;
 create table `t_article`(
-    `id`: BIGINT NOT NULL,
-    `group_id`: TINYINT UNSIGNED NOT NULL,
-    `author_id`: BIGINT NOT NULL,
-    `title`: VARCHAR(20) NOT NULL,
-    `abstract`: VARCHAR(100) NOT NULL,
-    `content`: TEXT NOT NULL,
-    `create_time`: BIGINT NOT NULL,
-    `last_update_time`: BIGINT NOT NULL,
+    `id` BIGINT NOT NULL,
+    `group_id` TINYINT UNSIGNED NOT NULL,
+    `author_id` BIGINT NOT NULL,
+    `title` VARCHAR(20) NOT NULL,
+    `abstract` VARCHAR(100) NOT NULL,
+    `content` TEXT NOT NULL,
+    `create_time` BIGINT NOT NULL,
+    `last_update_time` BIGINT NOT NULL,
     PRIMARY KEY(`id`),
     KEY index_group_id(`group_id`),
-    KEY index_author_id(`author_id`),
-)
+    KEY index_author_id(`author_id`)
+);
 
 create table `t_group`(
-    `id`: TINYINT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `name`: VARCHAR(20) NOT NULL,
-    `priority`: INT NOT NULL,
-    PRIMARY KEY(`id`),
-)
+    `id` TINYINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(20) NOT NULL,
+    `priority` INT NOT NULL,
+    PRIMARY KEY(`id`)
+);
 
 create table `t_user`(
-    `id`: BIGINT NOT NULL,
-    `username`: VARCHAR(100) NOT NULL,
-    `icon_url`: VARCHAR(100) NOT NULL,
-    `password`: VARCHAR(100) NOT NULL,
-    `is_admin`: TINYINT(1) NOT NULL,
-    `create_time`: BIGINT NOT NULL,
-    PRIMARY KEY(`id`),
-)
+    `id` BIGINT NOT NULL,
+    `username` VARCHAR(100) NOT NULL,
+    `icon_url` VARCHAR(100) NOT NULL,
+    `password` VARCHAR(100) NOT NULL,
+    `is_admin` TINYINT(1) NOT NULL,
+    `create_time` BIGINT NOT NULL,
+    PRIMARY KEY(`id`)
+);
